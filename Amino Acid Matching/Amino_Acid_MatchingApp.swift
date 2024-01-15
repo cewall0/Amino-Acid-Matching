13a0fd1d922a7007114d48d0854952cb6b8eb3cd
Chad Wallace <cewall0@gmail.com>
2024-01-15 13:58:21 -0500

    Initial Commit

---
+++ Amino Acid Matching/Amino_Acid_MatchingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Amino_Acid_MatchingApp.swift
+//  Amino Acid Matching
+//
+//  Created by Chad Wallace on 1/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Amino_Acid_MatchingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Amino Acid Matching/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Amino Acid Matching
+//
+//  Created by Chad Wallace on 1/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
